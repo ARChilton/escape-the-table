cd1effb36a34462f3b066ad9ff26feb4a77283ad
Adam Chilton <adam@remote-tech.co.uk>
2019-02-05 00:37:39 +0000

    added to public

---
+++ public/img/imgRegister.js
@@ -0,0 +1,19 @@
+import key1 from './key1.png'
+import key2 from './key2.png'
+import key3 from './key3.png'
+import key4 from './key4.png'
+import key5 from './key5.png'
+import key6 from './key6.png'
+import hint4 from './hint4.JPG'
+import hint7 from './hint7.JPG'
+
+export default {
+  key1,
+  key2,
+  key3,
+  key4,
+  key5,
+  key6,
+  hint4,
+  hint7,
+}
